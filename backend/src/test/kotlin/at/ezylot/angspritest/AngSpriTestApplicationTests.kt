1881ca0f83ae5d8874bf10bdccae2821942fad38
Florian Schöffl <florian.schoeffl@gmail.com>
2019-10-31 20:42:36 +0100

    Added example test

--- backend/src/test/kotlin/at/ezylot/angspritest/AngSpriTestApplicationTests.kt
+++ backend/src/test/kotlin/at/ezylot/angspritest/AngSpriTestApplicationTests.kt
@@ -1,5 +1,6 @@
 package at.ezylot.angspritest
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class AngSpriTestApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		Assertions.assertThat(true).isTrue()
 	}
 
 }
