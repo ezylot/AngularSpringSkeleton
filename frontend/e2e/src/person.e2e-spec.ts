bce0ca75645a516637e247059d1be1727a3d1d6f
Florian Schöffl <florian.schoeffl@gmail.com>
2019-11-18 23:31:30 +0100

    test

--- frontend/e2e/src/person.e2e-spec.ts
+++ frontend/e2e/src/person.e2e-spec.ts
@@ -10,12 +10,14 @@ describe("person regression test", () => {
 
     it("should compare list page with baseline", async () => {
         await navigateToListPage();
+        browser.waitForAngular();
         expect(await browser.imageComparison.checkElement(element(by.tagName("app-person-list")), "person-empty-list-page", { })).toBeLessThanOrEqual(0.1);
     });
 
 
     it("should compare creating page with baseline", async () => {
         await navigateToCreatePage();
+        browser.waitForAngular();
         expect(await browser.imageComparison.checkElement(element(by.tagName("app-person-edit")), "person-creation-page", { })).toBeLessThanOrEqual(0.1);
     });
 
@@ -47,20 +49,19 @@ describe("person regression test", () => {
         // Create form
         await element(by.css("form input[formControlName='firstName']")).sendKeys("Florian");
         await element(by.css("form input[formControlName='lastName']")).sendKeys("Schöffl");
-        await browser.sleep(200);
+
+        browser.waitForAngular();
         expect(await browser.imageComparison.checkElement(element(by.tagName("app-person-edit")), "person-creation-page-before-submit", { })).toBeLessThanOrEqual(0.1);
         await element(by.css("button[type=submit]")).click();
 
         // person list page
-        await browser.wait(protractor.ExpectedConditions.urlContains("list"), 10000);
-        await browser.sleep(50); // wait for snackbar to show
-
+        browser.waitForAngular();
         expect(await browser.imageComparison.checkElement(element(by.tagName("app-person-list")), "person-creation-page-after-submit", { })).toBeLessThanOrEqual(0.1);
         expect(await element(by.css("tr td:nth-of-type(2)")).getText()).toEqual("Florian");
         expect(await element(by.css("tr td:nth-of-type(3)")).getText()).toEqual("Schöffl");
 
         await element(by.css("tr td:nth-of-type(4) .ion-md-trash")).click();
-        await browser.sleep(50); // wait for snackbar to show
+        browser.waitForAngular();
         expect(await browser.imageComparison.checkElement(element(by.tagName("app-person-list")), "person-list-page-after-delete", { })).toBeLessThanOrEqual(0.1);
     });
 
